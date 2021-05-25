8417178fe0a283cd67364f8a70fbb6fff6717c7d
CwhiteKJ <dw696969@naver.com>
2021-05-25 19:56:33 +0900

    모듈로 구조 변경

---
+++ NonMannerDB/nonmanner.py
@@ -0,0 +1,32 @@
+import aiohttp
+
+
+class BadInput:
+    def __init__(self, msg="Bad Input"):
+        super().__init__(msg)
+
+
+class NonManner:
+    def __init__(self):
+        self.url = "https://userdb.ourmc.space/api/v1/report"
+
+    async def search_by_name(self, username: str, skip=0, limit=100):
+        return await self.__aiohttp_request("search", username, skip, limit)
+
+    async def get_all(self, skip=0, limit=100):
+        return await self.__aiohttp_request("all", skip=skip, limit=limit)
+
+    async def get_count(self, uuid: str):
+        return await self.__aiohttp_request("reportcount", uuid)
+
+    async def get_user(self, uuid: str):
+        return await self.__aiohttp_request("user", uuid)
+
+    async def __aiohttp_request(self, point: str, user: str = "", skip=0, limit=100):
+        params = {"skip": skip, "limit": limit}
+        if point not in ["reportcount", "user", "all", "search"]:
+            raise BadInput("Cannot find endpoint that named {point}")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{self.url}/{point}/{user}", params=params) as result:
+                return await result.json(content_type=None)
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='NonMannerDB',
+    version='1.0.1',
+    description='우마공DB 전용 라이브러리',
+    long_description=open('README.md', encoding='utf-8').read(),
+    author='WhiteKJ',
+    author_email='whitekj1221@gmail.com',
+    url='https://github.com/CwhiteKJ/NonMannerDB',
+    install_requires=['aiohttp'],
+    python_requires='>=3',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
