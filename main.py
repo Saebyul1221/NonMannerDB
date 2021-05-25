74235ba12a85a388dce832a9faba9b51937834a0
CwhiteKJ <dw696969@naver.com>
2021-05-25 18:41:25 +0900

    fix: private method aiohttp_request

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ class NonManner():
         self.url = "https://userdb.ourmc.space/api/v1/report"
 
     async def search_by_name(self, username: str, skip=0, limit=100):
-        return await self.aiohttp_request("search", username, skip, limit)
+        return await self.__aiohttp_request("search", username, skip, limit)
 
     async def get_all(self, skip=0, limit=100):
-        return await self.aiohttp_request("all", "nobody", skip, limit)
+        return await self.__aiohttp_request("all", "nobody", skip, limit)
 
     async def get_count(self, uuid: str):
-        return await self.aiohttp_request("reportcount", uuid)
+        return await self.__aiohttp_request("reportcount", uuid)
 
     async def get_user(self, uuid: str):
-        return await self.aiohttp_request("user", uuid)
+        return await self.__aiohttp_request("user", uuid)
 
-    async def aiohttp_request(self, point: str, user: str, skip=0, limit=100):
+    async def __aiohttp_request(self, point: str, user: str, skip=0, limit=100):
         params = {"skip": skip, "limit": limit}
         async with aiohttp.ClientSession() as session:
             if point is "reportcount":
